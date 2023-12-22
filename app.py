17301fa54b02e069206c364dbbeeccb0e03922a5
Syaikhul Shafwan R <heymepoo@gmail.com>
2023-12-22 10:25:27 +0700

    sdasdasd

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, jsonify, url_for
 from PSI.Controller import main
+from PSI.Controller import main2
 import numpy as np
 
 app = Flask(__name__)
@@ -15,6 +16,17 @@ def process_array():
 
     # Panggil fungsi Python untuk memproses array
     result = main(mergedData)
+
+    # Return the result as JSON
+    return jsonify(result)
+
+@app.route('/SAW', methods=['POST'])
+def process_array2():
+    data = request.json
+    # Ambil data JSON dari permintaan
+    mergedData= data.get('array') 
+    # Panggil fungsi Python untuk memproses array
+    result = main2()
 
     # Return the result as JSON
     return jsonify(result)
